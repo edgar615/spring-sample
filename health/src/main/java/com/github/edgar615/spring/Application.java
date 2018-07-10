011d10dc7e99df39b038cfdbe700bab2a66e4f92
edgar615 <edgar615@gmail.com>
2018-07-10 19:24:10 +0800

    web timed

--- health/src/main/java/com/github/edgar615/spring/Application.java
+++ health/src/main/java/com/github/edgar615/spring/Application.java
@@ -1,5 +1,6 @@
 package com.github.edgar615.spring;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {"com.github.edgar615.**"})
 @RestController
+//@Timed
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
+  @Timed("test.index")
   @RequestMapping("/")
   public String home() {
     return "Hello world";
