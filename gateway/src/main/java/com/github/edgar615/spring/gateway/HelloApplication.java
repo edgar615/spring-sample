a406429e43d620dfa35c0509605cf565bc926db6
Edgar <edgar615@gmail.com>
2018-07-16 22:11:28 +0800

    gatway

---
+++ gateway/src/main/java/com/github/edgar615/spring/gateway/HelloApplication.java
@@ -0,0 +1,20 @@
+package com.github.edgar615.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HelloApplication {
+  @Bean
+  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    return builder.routes().route(r -> r.path("/baidu").uri("http://baidu.com:80/")).build();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloApplication.class, args);
+  }
+}
+
