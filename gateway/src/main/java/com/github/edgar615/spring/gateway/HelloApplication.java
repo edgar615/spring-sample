d11792065ae38e656ed604147941d2bbe3403643
Edgar <edgar615@gmail.com>
2018-07-17 21:48:11 +0800

    gateway

--- gateway/src/main/java/com/github/edgar615/spring/gateway/HelloApplication.java
+++ gateway/src/main/java/com/github/edgar615/spring/gateway/HelloApplication.java
@@ -2,16 +2,35 @@ package com.github.edgar615.spring.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HelloApplication {
-  @Bean
-  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-    return builder.routes().route(r -> r.path("/baidu").uri("http://baidu.com:80/")).build();
-  }
+//  @Bean
+//  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//    return builder.routes().route(r -> r.path("/baidu")
+//            .filters(f ->
+//                    f.addResponseHeader("X-AnotherHeader", "baz"))
+//            .uri("http://baidu.com:80/"))
+//            .route(r -> r.path("/api")
+//                    .filters(f ->
+//                            f.addResponseHeader("X-AnotherHeader", "baz"))
+//                    .uri("lb://consul-sample")).build();
+//  }
+//
+//  @Bean
+//  public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient,
+//                                                                      DiscoveryLocatorProperties properties) {
+//    return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+//  }
 
   public static void main(String[] args) {
     SpringApplication.run(HelloApplication.class, args);
