daa16e12cf069e2d94a11c4f10c4a42ed9995609
edgar615 <edgar615@gmail.com>
2018-08-06 18:05:11 +0800

    logging

---
+++ logging/src/main/java/com/github/edgar615/spring/logging/Application.java
@@ -0,0 +1,31 @@
+package com.github.edgar615.spring.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  Logger logger = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @GetMapping("/")
+  public String hello() {
+    logger.trace("A TRACE Message");
+    logger.debug("A DEBUG Message");
+    logger.info("An INFO Message");
+    logger.warn("A WARN Message");
+    logger.error("An ERROR Message");
+    return "hello";
+
+  }
+}
+
---
+++ logstash/src/main/java/com/github/edgar615/spring/logging/Application.java
@@ -0,0 +1,35 @@
+package com.github.edgar615.spring.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  Logger logger = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @GetMapping("/")
+  public String hello() {
+    MDC.put("x-request-id", UUID.randomUUID().toString());
+    logger.trace("A TRACE Message");
+    logger.debug("A DEBUG Message");
+    logger.info("An INFO Message");
+    logger.warn("A WARN Message");
+    logger.error("An ERROR Message");
+    return "hello";
+
+  }
+}
+
